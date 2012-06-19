74684433fd5ea85dfbf5defa82d75a1c508742d9
Matt Basta <me@mattbasta.com>
2012-06-19 15:33:45 -0700

    Updated fabric with a simple install script

--- fabfile.py
+++ fabfile.py
@@ -4,7 +4,7 @@ Basic fabfile to setup environment for running tests
 import functools
 import os
 
-from fabric.api import local
+from fabric.api import local, run
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
 
@@ -20,3 +20,18 @@ def test(pdb=False):
         cmd += ' --pdb --pdb-failures -s'
 
     local(cmd)
+
+
+def install():
+    # Setup virtualenv if it's not already set up.
+    if not run('which workon'):
+        run('easy_install virtualenv')
+        run('wget http://bitbucket.org/dhellmann/virtualenvwrapper/raw/'
+            'f31869779141/virtualenvwrapper_bashrc -O ~/.virtualenvwrapper')
+        run('mkdir ~/.virtualenvs')
+        run('export WORKON_HOME=~/.virtualenvs')
+        run('source ~/.virtualenvwrapper')
+
+    run('pip install -r requirements.txt')
+    run('git submodule update --init')
+
